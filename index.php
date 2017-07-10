497a5a19eb67ca64d17322c5b9fb0ce3286ee2e1
Dylan Reuter <djreuter45@gmail.com>
2017-07-10 16:45:59 -0600

    Added img and stylesheet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Jack</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h1>About Me</h1>
+		<p>My name is Jack, I am 24 years old and I'm from Albuquerque, NM. I graduated from UNM in 2016 and have
+			worked in sales and financial services during the past year. I am a soccer fan, musician, and outdoor
+			enthusiast. In my spare time I can be found outside either slacklining, longboarding, snowboarding, or playing
+			soccer. Or I may be inside playing guitar, reading, or watching Netflix.
+		</p>
+		<img id="me" src="images/me-resized.jpg" alt="me">
+	</body>
+</html>
